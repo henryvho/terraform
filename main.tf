25d6b26251b9096c608b269b74dce1b0fb880643
henryvho <henryvho@yahoo.com>
2019-07-13 00:24:42 -0700

    0.1

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "example" {
 	user_data = <<-EOF
 				#!/bin/bash
 				echo "Hello World" > index.html
-				nohup busybox httpd -f -p 8080 &
+				nohup busybox httpd -f -p "${var.server_port}" &
 				EOF	
 
 	tags = {
@@ -23,8 +23,8 @@ resource "aws_security_group" "instance" {
 	name = "terraform-example-sg"
 
 	ingress {
-		from_port = 8080
-		to_port = 8080
+		from_port = "${var.server_port}"
+		to_port = "${var.server_port}"
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
@@ -32,4 +32,9 @@ resource "aws_security_group" "instance" {
 
 variable "server_port" {
 	description = "web server listening port"
+	default = 8080
+}
+
+output "public_ip" {
+	value = "${aws_instance.example.public_ip}"
 }
